494afb1c01a319f7343e6bdc183bd103fed2a5fd
JaronYankey <127556634+JaronYankey@users.noreply.github.com>
2025-03-30 21:29:47 -0400

    got player class somewhat ready for abilites
    have a basic way of setting then running the special ability selected by the player.

--- Scripts/nimble_power.gd
+++ Scripts/nimble_power.gd
@@ -6,4 +6,5 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		# Modify player stats or activate stats.
 		body.speed = 400
+		body.set_power("nimble")
 		queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 # Player movement speed
 var speed = 175
+var power_ready: bool = false
+var power_type: String
+var ability = func(): print("No ability selected!!") # Defualt message for now
 
 
 func get_input():
@@ -11,3 +14,19 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	if Input.is_action_pressed("special_power"):
+		# Must use .call() on the var ability to get the function to run.
+		ability.call() # Run the appropriate function for the power the player has.
+	
+func set_power(power: String):
+	if power == "nimble":
+		ability = dash # No parentheses because its assignment, not a call to dash()
+
+func dash():
+	print("I DASHED FORWARD!!!")
+	
+func riot_charge():
+	pass
+	
+func bullet_frenzy():
+	pass
