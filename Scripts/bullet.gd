3f0e553edbc0eac2fb63c238df2e46ae2e8c5aca
JaronYankey <127556634+JaronYankey@users.noreply.github.com>
2025-04-10 07:28:17 -0400

    tweaks

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+@onready var audio_player = $AudioStreamPlayer2D
 var speed: float = 800
 var velocity: Vector2 
 
@@ -19,5 +20,5 @@ func _on_body_entered(body):
 	# Zombies will have health so, they wont be one shot. This is temporary.
 	if body.is_in_group("basic_zombie"):
 		body.queue_free() # Don't know if we have to "set defered physics" at some point? Like we did in 
-	# dodge the creeps. I don;t know if it will matter for preformance later on.
+	# dodge the creeps. I don't know if it will matter for preformance later on.
 	queue_free() # Destroy bullet on impact
