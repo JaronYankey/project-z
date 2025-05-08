64fe484b3360fde7c206ca14f9574f16164a528a
JaronYankey <127556634+JaronYankey@users.noreply.github.com>
2025-05-07 22:32:18 -0400

    Beginning new terry movement changes

--- Scripts/terry.gd
+++ Scripts/terry.gd
@@ -1,16 +1,25 @@
 extends CharacterBody2D
 
+enum CAT_STATES {content, scared, hungry, happy}
+
 @export var pull_force = 500.0
 
 @onready var terry_collison = $TerryCollisionShape
 @onready var audio_player = $AudioStreamPlayer2D
 @onready var health_bar = $ProgressBar
 @onready var leash = $LeashLine
+@onready var idle_timer = $IdleTimer
+@onready var moving_timer = $MovingTimer
 
 var is_pulled = false
 var leashed = false
 var player_ref: Node2D = null
 var colliding_objects: Array = []
+var vec_dirs: Array = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
+
+var walking = false
+var idle = true
+var walk_idle_toggle = true
 
 func _ready():
 	player_ref = get_tree().get_first_node_in_group("player")
@@ -23,8 +32,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	
-	# TODO FIX TERRY MOVEMENT. I CHANGED FROM A PHYSICS BODY2D TO A CHARACTERBODY2D
-	# MOVEMENT IS BROKEN NOW, BUT IN THE LONG TERM SHOULD SERVE BETTER FOR HIS AI MOVEMENT.
+	move(delta)
 	if is_pulled and player_ref:
 		var direction = (player_ref.global_position - global_position).normalized()
 		var pull_strength = 200  # Lower = heavier
@@ -43,8 +51,43 @@ func _physics_process(delta):
 		]
 
 
+func move(delta):
+	var state = cat_state()
+	var direction
+	var speed 
+	
+	if walking == false and idle == true and !walk_idle_toggle:
+		direction = Vector2.ZERO
+		speed = 1 # should equate to no movement.
+		idle_timer.wait_time = randi_range(2, 4)
+		idle = false
+		idle_timer.start()
+	# TODO: Not working 
+	if state == CAT_STATES.content and idle == true:
+		direction = vec_dirs.pick_random()
+		speed = 300
+		moving_timer.wait_time = randi_range(2, 4)
+		idle = false
+		walking = true
+		walk_idle_toggle = !walk_idle_toggle # Brain not working, hopefilly toggles 
+		moving_timer.start()
+		
+	if direction:
+		velocity = direction * speed
+		move_and_slide()
 
 
+# What is the cat's current motive?
+func cat_state():
+	
+	if is_content(): # Criteria for "Content" has been met
+		return CAT_STATES.content
+
+
+func is_content():
+	# If criteria for being content is met
+	return true
+	
 func check_collision_type(objects: Array):
 	#TODO: This works. But it calls is every frame so terry dies SUPER quick. 
 	# NOTE: reducing the damage output to something very small seems okay. 
@@ -69,3 +112,13 @@ func _on_terry_body_zone_body_entered(body):
 
 func _on_terry_body_zone_body_exited(body):
 	call_deferred("remove_from_collision_area_array", body)
+
+
+func _on_moving_timer_timeout():
+	idle = true
+	walking = false
+	# direction global to change to Vector2.ZERO?
+
+
+func _on_idle_timer_timeout():
+	idle = true
--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -20,13 +20,15 @@ func _ready():
 	targets = [terry, terry, terry, player]
 	health_bar.value = 100
 	
-	target = targets.pick_random()
+	#target = targets.pick_random()
 func _physics_process(delta):
 	if target: # Ensures we dont have a null value
 		var direction = (target.global_position - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
 		animation.play()
+	else:
+		animation.stop()
 
 func _process(delta):
 	if health_bar.value <= 0:
@@ -44,3 +46,21 @@ func hit(damage: int):
 
 func _on_death_sound_finished():
 	queue_free()
+
+
+func _on_line_of_sight_area_entered(area):
+	pass
+
+
+func _on_line_of_sight_area_exited(area):
+	pass
+
+
+func _on_line_of_sight_body_entered(body):
+	if body.is_in_group("living"):
+		target = body
+
+
+func _on_line_of_sight_body_exited(body):
+	if body.is_in_group("living"):
+		target = null
